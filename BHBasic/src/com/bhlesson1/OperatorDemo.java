5da6b1728b00db7490da412e2f0fc082644b1481
YUQILove <17705990930@qq.com>
2021-05-11 00:50:37 +0800

    黑马1.2

---
+++ BHBasic/src/com/bhlesson1/OperatorDemo.java
@@ -0,0 +1,9 @@
+package com.bhlesson1;
+
+public class OperatorDemo {
+    public static void main(String[] args) {
+        System.out.println("sout快捷打印");
+
+        System.out.println("我爱java");
+    }
+}
